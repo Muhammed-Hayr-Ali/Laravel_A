99a9fcdd3661ec9ff92386b6f27e331735124ebc
Muhammed-Hayr-Ali <m.thelord963@gmail.com>
2024-02-20 22:26:46 +0300

    users  is ok!!

--- resources/views/dashboard/user/components/images.blade.php
+++ resources/views/dashboard/user/components/images.blade.php
@@ -1,13 +1,13 @@
 @if (isset($user->profile) && $user->profile != null)
     <div class="image-group">
         <div class="col-12 border border-secondary-subtle rounded-2 text-center ">
-            <a href="{{ asset($user->profile) }}" class="profile-popup" data-lightbox="roadtrip">
+            <a href="{{ asset($user->profile) }}" class="image-popup" data-lightbox="roadtrip">
                 <img src="{{ asset($user->profile) }}" alt="">
             </a>
             <div class="container">
                 <div class="row align-items-center">
-                    <div class="col-auto m-0 p-0"><button class="delete btn btn-circle" data-id="{{ $user->id }}"
-                            data-name="{{ basename($user->profile) }}">
+                    <div class="col-auto m-0 p-0"><button class="delete btn btn-circle"
+                            data-name="{{ basename($user->profile) }}"   data-id="{{ basename($user->id) }}">
                             <i class="fa fa-trash" aria-hidden="true"></i>
                         </button>
                     </div>
